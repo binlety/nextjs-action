d716912c94475e15abedfd40f832eb727c54e424
lib <libin@zhaojiafang.com>
2021-11-24 18:31:38 +0800

    fixed:alias

--- components/Layouts/BasicLayout.js
+++ components/Layouts/BasicLayout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Header, Sidebar, Content, Footer, Sidenav, Nav, Dropdown, Message, Avatar  } from 'rsuite'
 import styles from './index.module.css'
-import IconFont from '/components/IconFont';
+import IconFont from '@/components/IconFont';
 
 const Layout = ({ children }) => {
   return (
@@ -52,38 +52,6 @@ const Layout = ({ children }) => {
   //    <div className={styles.section}>{children}</div>
   // </div>
 
-//   <div className='wrap'>
-//   <div className='aside' style={{width: 240}}>
-//      <Sidenav defaultOpenKeys={['3', '4']} activeKey="1">
-//        <Sidenav.Body>
-//          <Nav>
-//            <Nav.Item eventKey="1" >
-//              Dashboard
-//            </Nav.Item>
-//            <Nav.Item eventKey="2" >
-//              User Group
-//            </Nav.Item>
-//            <Dropdown eventKey="3" title="Advanced" >
-//              <Dropdown.Item eventKey="3-1">Geo</Dropdown.Item>
-//              <Dropdown.Item eventKey="3-2">Devices</Dropdown.Item>
-//              <Dropdown.Item eventKey="3-3">Loyalty</Dropdown.Item>
-//              <Dropdown.Item eventKey="3-4">Visit Depth</Dropdown.Item>
-//            </Dropdown>
-//            <Dropdown eventKey="4" title="Settings" >
-//              <Dropdown.Item eventKey="4-1">Applications</Dropdown.Item>
-//              <Dropdown.Item eventKey="4-2">Channels</Dropdown.Item>
-//              <Dropdown.Item eventKey="4-3">Versions</Dropdown.Item>
-//              <Dropdown.Menu eventKey="4-5" title="Custom Action">
-//                <Dropdown.Item eventKey="4-5-1">Action Name</Dropdown.Item>
-//                <Dropdown.Item eventKey="4-5-2">Action Params</Dropdown.Item>
-//              </Dropdown.Menu>
-//            </Dropdown>
-//          </Nav>
-//        </Sidenav.Body>
-//      </Sidenav>
-//   </div>
-//   <div className='section'  style={{display:'flex', justifyContent:'center', alignItems:'center'}}>content</div>
-// </div>
   )
 };
 
